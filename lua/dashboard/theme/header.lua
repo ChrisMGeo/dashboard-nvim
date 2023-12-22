aec7822d6aaf052207525311f61a1b8a8a2c5af6
Chris George <chrismathew98@gmail.com>
2023-12-21 22:05:26 -0500

    Modified header to export get_header and use it in generate_header

--- lua/dashboard/theme/header.lua
+++ lua/dashboard/theme/header.lua
@@ -103,25 +103,47 @@ local function week_header(concat, append)
   return tbl
 end
 
-local function generate_header(config)
-  if not vim.bo[config.bufnr].modifiable then
-    vim.bo[config.bufnr].modifiable = true
-  end
+local function get_header(config)
   if not config.command then
-    local header = config.week_header
+    local header_config = config.week_header
         and config.week_header.enable
         and week_header(config.week_header.concat, config.week_header.append)
       or (config.header or default_header())
-    api.nvim_buf_set_lines(config.bufnr, 0, -1, false, utils.center_align(header))
-
-    for i, _ in ipairs(header) do
-      vim.api.nvim_buf_add_highlight(config.bufnr, 0, 'DashboardHeader', i - 1, 0, -1)
-    end
-    return
+    local lines = header_config.lines or header_config
+    local alignment = header_config.alignment or 'center'
+    local in_alignment = header_config.in_alignment or 'left'
+    return { lines = lines, alignment = alignment, in_alignment = in_alignment }
   end
 
   local empty_table = utils.generate_empty_table(config.file_height + 4)
-  api.nvim_buf_set_lines(config.bufnr, 0, -1, false, utils.center_align(empty_table))
+  return { lines = empty_table, alignment = 'center', in_alignment = 'center' }
+end
+
+local function generate_header(config)
+  local header = get_header(config)
+  if not config.command then
+    local extents = utils.buf_set_aligned_lines(
+      header.lines,
+      config.bufnr,
+      0,
+      -1,
+      false,
+      header.alignment,
+      header.in_alignment
+    )
+    for i, extent in pairs(extents) do
+      vim.api.nvim_buf_add_highlight(
+        config.bufnr,
+        0,
+        'DashboardHeader',
+        i - 1,
+        extent.col_start,
+        extent.col_end
+      )
+    end
+    return
+  end
+  api.nvim_buf_set_lines(config.bufnr, 0, -1, false, utils.center_align(header.lines))
   local preview = require('dashboard.preview')
   preview:open_preview({
     width = config.file_width,
@@ -132,4 +154,5 @@ end
 
 return {
   generate_header = generate_header,
+  get_header = get_header,
 }
